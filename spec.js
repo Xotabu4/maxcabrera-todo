c767db9067d74c8ab0888a35e4cd88df2033ab61
Alexander Khotemskoy <Xotabu4@gmail.com>
2016-11-10 21:08:23 +0200

    Example of protractor functions
    Example of protractor functions

--- spec.js
+++ spec.js
@@ -1,7 +1,23 @@
-describe('suite', function () {
+//JasmineJS 2
 
-    it('test', function () {
-        browser.get('http://maxcabrera.com/code/todo-list/')
+let URL = 'http://maxcabrera.com/code/todo-list/'
+
+describe('TODO site', function () {
+    
+    it('should have correct title', function () {
+        browser.get(URL)
         expect(browser.getTitle()).toBe('Angular2 Seed')
+    })
+
+    it('should create new note', function () {
+        browser.get(URL)
+        let webelement = $('input.enter-todo')
+        webelement.sendKeys('New Shiny note')
+        webelement.submit()
+
+        let notes = $$('todo-list .small-12')
+
+        //expect(notes.getText()).not.toContain('New Shiny note')
+        expect(notes.getText()).toContain('New Shiny note')
     })
 })
